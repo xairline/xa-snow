a789c9ad61f480b68a640d6c3d7f6213901cd8f9
Holger Teutsch <holger.a.teutsch@gmail.com>
2024-11-14 14:53:29 +0100

    reformat interpolation table

--- services/phys_2_xplane.go
+++ services/phys_2_xplane.go
@@ -24,65 +24,29 @@ func NewPhys2XPlane(logger logger.Logger) Phys2XPlane {
 // convert snow depth from grib(m) to xplane snow_now
 // interpolation table
 var (
-	snowDepthTabLowerLimit float32 = 0.01
-	snowDepthTabUpperLimit float32 = 0.25
-	snowDepthTab                   = []float32{
-		snowDepthTabLowerLimit,
-		0.02,
-		0.03,
-		0.05,
-		0.10,
-		0.20,
-		snowDepthTabUpperLimit,
-	} // depth
-
-	snowNowTabLowerLimit float32 = 0.05
-	snowNowTabUpperLimit float32 = 0.90
-	snowNowTab                   = []float32{
-		snowNowTabUpperLimit,
-		0.70,
-		0.60,
-		0.30,
-		0.15,
-		0.06,
-		snowNowTabLowerLimit,
-	} // snowNow
-
-	snowAreaWidthTabLowerLimit float32 = 0.25
-	snowAreaWidthTabUpperLimit float32 = 0.33
-	snowAreaWidthTab                   = []float32{
-		snowAreaWidthTabUpperLimit,
-		snowAreaWidthTabLowerLimit,
-		snowAreaWidthTabLowerLimit,
-		snowAreaWidthTabLowerLimit,
-		snowAreaWidthTabLowerLimit,
-		0.29,
-		snowAreaWidthTabLowerLimit,
-	}
+	snowDepthTab     = []float32{0.01, 0.02, 0.03, 0.05, 0.10, 0.20, 0.25}
+	snowNowTab       = []float32{0.90, 0.70, 0.60, 0.30, 0.15, 0.06, 0.05}
+	snowAreaWidthTab = []float32{0.25, 0.25, 0.25, 0.25, 0.25, 0.29, 0.33}
 )
 
 func (p2x *phys2XPlane) SnowDepthToXplaneSnowNow(depth float32) (float32, float32) {
-	if depth >= snowDepthTabUpperLimit {
-		return snowNowTabLowerLimit, snowAreaWidthTabUpperLimit //snowAreaWidthTabLowerLimit
+	if depth >= snowDepthTab[len(snowDepthTab)-1] {
+		return snowNowTab[len(snowNowTab)-1], snowAreaWidthTab[len(snowAreaWidthTab)-1]
 	}
 
-	if depth <= snowDepthTabLowerLimit {
-		return 1.2, snowAreaWidthTabLowerLimit //snowAreaWidthTabUpperLimit
+	if depth <= snowDepthTab[0] {
+		return 1.2, snowAreaWidthTab[0]
 	}
 
 	// piecewise linear interpolation
 	snowNowValue := float32(1.2)
-	snowAreaWidthValue := float32(0.25)
+	snowAreaWidthValue := snowAreaWidthTab[0]
 	for i, sd0 := range snowDepthTab {
 		sd1 := snowDepthTab[i+1]
 		if sd0 <= depth && depth < sd1 {
 			x := (depth - sd0) / (sd1 - sd0)
 			snowNowValue = snowNowTab[i] + x*(snowNowTab[i+1]-snowNowTab[i])
-			// apply snow area width interpolation
-			// only when we have more than 20cm of snow
-			if depth >= 0.2 {
-				snowAreaWidthValue = snowAreaWidthTab[i] + x*(snowAreaWidthTab[i+1]-snowAreaWidthTab[i])
-			}
+			snowAreaWidthValue = snowAreaWidthTab[i] + x*(snowAreaWidthTab[i+1]-snowAreaWidthTab[i])
 			break
 		}
 	}
