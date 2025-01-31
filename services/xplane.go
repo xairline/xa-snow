986ec6b14d1cb0dca250a9d0fd207d17e56e86e7
Holger Teutsch <holger.a.teutsch@gmail.com>
2025-01-31 12:44:27 +0100

    restore xplane.go

--- services/xplane.go
+++ services/xplane.go
@@ -106,8 +106,6 @@ func NewXplaneService(
 		xplaneSvcLock.Lock()
 		defer xplaneSvcLock.Unlock()
 
-		InitXaSnowC()	// init the C environment
-
 		systemPath := utilities.GetSystemPath()
 		pluginPath := filepath.Join(systemPath, "Resources", "plugins", "XA-snow")
 		_, cancelFunc := context.WithCancel(context.Background())
@@ -312,8 +310,6 @@ func (s *xplaneService) flightLoop(
 			return -1
 		}
 
-		la_sd := LegacyAirportSnowDepth(s.snowDepth)
-		s.Logger.Infof("LegacyAirportSnowDepth: in %0.2f, out: %0.2f", s.snowDepth, la_sd)
 		s.snowNow, s.rwySnowCover, s.iceNow = s.p2x.SnowDepthToXplaneSnowNow(s.snowDepth)
 	}
 
