04e3911caf1aa97db35b96cad606cbdad5f53da9
Holger Teutsch <holger.a.teutsch@gmail.com>
2025-01-31 12:31:05 +0100

    build plugin on native platform

--- dotenv_msys2.sh
+++ dotenv_msys2.sh
@@ -13,7 +13,8 @@ then
 fi
 
 export CGO_CFLAGS="-DIBM=1 -O2"
-export CGO_LDFLAGS="-L"$this_dir"/Libraries/Win -lXPLM_64"
+export CGO_CXXFLAGS="-std=c++20 -DIBM=1 -O2 -I $this_dir/SDK/CHeaders/XPLM"
+export CGO_LDFLAGS="-L"$this_dir"/Libraries/Win -lXPLM_64 -static-libgcc -static -lstdc++"
 export GOOS=windows
 export GOARCH=amd64
 export CGO_ENABLED=1
